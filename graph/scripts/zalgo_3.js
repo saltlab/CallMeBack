8e2fbe96906b051ebfe716e50dc5944ef30a00cc
Keheliya Gallaba <keheliya.gallaba@gmail.com>
2014-09-04 17:41:28 -0700

    sample problem

--- graph/scripts/zalgo_3.js
+++ graph/scripts/zalgo_3.js
@@ -1,6 +1,6 @@
 function getData (args, cb) {
-    if (!args) { return cb(new Error('args required')); }
-    else { return cb(new Error('args required')); }
+    if (!args || a()) { return cb(new Error('args required')); }
+    else { cb(new Error('args required')); }
     //if (!args.id) { return cb(new Error('args.id required')); }
 
    	setTimeout(cb,100);
@@ -8,7 +8,55 @@ function getData (args, cb) {
 
 render = function (params) {
 	console.log(params);
-}
+};
+
+f();
+
+f(a()+b());
+
+c = a();
 
 getData(null,render);
 getData('students', render);
+
+if (f(a()+b()) === a()) {
+    result = result + to_char;
+    x++;
+    a();
+} else {
+    result = result + clear_char;
+    b();
+}
+
+//for (var i = d(); a()>b(); c()+d()) {
+//    console.log(i);
+//    // more statements
+//}
+//
+//while (a()) {
+//    n++;
+//    x += n;
+//}
+
+//switch (expr()) {
+//    case "Oranges":
+//        console.log("Oranges are $0.59 a pound.");
+//        break;
+//    case "Apples":
+//        console.log("Apples are $0.32 a pound.");
+//        break;
+//    case "Bananas":
+//        console.log("Bananas are $0.48 a pound.");
+//        break;
+//    case "Cherries":
+//        console.log("Cherries are $3.00 a pound.");
+//        break;
+//    case "Mangoes":
+//    case "Papayas":
+//        console.log("Mangoes and papayas are $2.79 a pound.");
+//        break;
+//    default:
+//        console.log("Sorry, we are out of " + expr + ".");
+//}
+//
+//console.log("Is there anything else you'd like?");
