1a50359b8d84f8f6d6f04bfca9d056ff14e9cbef
Keheliya Gallaba <keheliya.gallaba@gmail.com>
2014-09-15 12:19:01 -0700

    refactoring

--- graph/callsitetrack.js
+++ graph/callsitetrack.js
@@ -6,7 +6,7 @@ var filename = process.argv[2];
 var src = fs.readFileSync(filename);
 var ast = esprima.parse(src, { tolerant: true, loc: true, range: true });
 
-
+var utils = require('./utils.js');
 
 var bindings = require('./javascript-call-graph/bindings'),
 astutil = require('./javascript-call-graph/astutil'),
@@ -45,7 +45,7 @@ falafel({ source: src,
 
 	if (node.type === 'FunctionExpression' || node.type === 'FunctionDeclaration') {		
 		var id = 'cluster'+makeId(node.type,node.loc);
-		var newlabel = concoctFunctionName (node);
+		var newlabel = utils.concoctFunctionName (node);
 		
 		var params = [];
 		node.params.forEach(function (param) {
@@ -140,109 +140,6 @@ function leave(node){
 }
 
 
-
-
-
-// uses the surrounding code to generate a reasonable name for a function
-function concoctFunctionName (node) {
-	var name = undefined;
-
-	if (node.type === 'FunctionDeclaration') {
-		// function xxx() { }
-		//  -> "xxx"
-		name = node.id.name;
-	} else if (node.type === 'FunctionExpression') {
-		if (node.id) {
-			// (function xxx() { })
-			//  -> "xxx"
-			name = node.id.name;
-		} else if (node.parent.type === 'VariableDeclarator') {
-			// var xxx = function () { }
-			//  -> "xxx"
-			name = node.parent.id.name;
-		} else if (node.parent.type === 'AssignmentExpression') {
-			var left = node.parent.left;
-			if (left.type === 'MemberExpression' && !left.computed) {
-				if (left.object.type === 'MemberExpression' && !left.object.computed) {
-					if (left.object.property.type === 'Identifier' && left.object.property.name === 'prototype') {
-						// yyy.prototype.xxx = function () { }
-						//  -> "yyy.xxx"
-						name = left.object.object.name + '.' + left.property.name;
-					}
-				}
-			}
-			else if (left.type === 'Identifier') {
-				// xxx = function () { }
-				//  -> "xxx"
-				name = left.name;
-			}
-		} else if (node.parent.type === 'CallExpression') {
-			// look, I know this is a regexp, I'm just sick of parsing ASTs
-			if (/\.on$/.test(node.parent.callee.source())) {
-				var args = node.parent.arguments;
-				if (args[0].type === 'Literal' && typeof args[0].value === 'string') {
-					// .on('event', function () { })
-					//  -> "('event' handler)"
-					name = "('" + args[0].value + "' handler)";
-				}
-			} else if (node.parent.callee.type === 'Identifier') {
-				if (['setTimeout', 'setInterval'].indexOf(node.parent.callee.name) !== -1) {
-					// setTimeout(function () { }, xxx)
-					// setInterval(function () { }, xxx)
-					//  -> "timer handler"
-					name = 'timer handler';
-					if (node.parent.arguments[1] && node.parent.arguments[1].type === 'Literal' && typeof node.parent.arguments[1].value === 'number') {
-						// setTimeout(function () { }, 100)
-						// setInterval(function () { }, 1500)
-						//  -> "timer handler (100ms)"
-						//  -> "timer handler (1.5s)"
-						if (node.parent.arguments[1].value < 1000) {
-							name += ' (' + node.parent.arguments[1].value + 'ms)';
-						} else {
-							name += ' (' + (node.parent.arguments[1].value / 1000) + 's)';
-						}
-					}
-					name = '(' + name + ')';
-				} else {
-					// xxx(function () { })
-					//  -> "('xxx' callback)"
-					name = "('" + node.parent.callee.source() + "' callback)";
-				}
-			} else if (node.parent.callee.type === 'MemberExpression') {
-				if (node.parent.callee.property.type === 'Identifier') {
-					// xxx.yyy(..., function () { }, ...)
-					//  -> "('yyy' callback)"
-					name = "('" + node.parent.callee.property.name + "' callback)";
-				}
-			}
-		} else if (node.parent.type === 'Property') {
-			// { xxx: function () { } }
-			//  -> "xxx"
-			name = node.parent.key.name || node.parent.key.value;
-			if (name !== undefined) {
-				if (node.parent.parent.type === 'ObjectExpression') {
-					var obj = node.parent.parent;
-					if (obj.parent.type === 'VariableDeclarator') {
-						// var yyy = { xxx: function () { } }
-						//  -> "yyy.xxx"
-						name = obj.parent.id.name + '.' + name;
-					} else if(obj.parent.type === 'AssignmentExpression') {
-						var left = obj.parent.left;
-						if (left.type === 'MemberExpression' && !left.computed) {
-							if (left.object.type === 'Identifier' && left.property.name === 'prototype') {
-								// yyy.prototype = { xxx: function () { } }
-								//  -> "yyy.xxx"
-								name = left.object.name + '.' + name;
-							}
-						}
-					}
-				}
-			}
-		}
-	}
-	return name;
-};
-
 // function isVarDefined(varname, scopeChain){
 // 	for (var i = 0; i < scopeChain.length; i++){
 // 		var scope = scopeChain[i];
@@ -316,5 +213,5 @@ cg.edges.iter(function (call, fn) {
 	}
 });
 
-console.dir(g);
+//console.dir(g);
 console.log(dot.write(g));
--- graph/utils.js
+++ graph/utils.js
@@ -26,6 +26,107 @@ function makeLabel(node, src) {
     }
 }
 
+// uses the surrounding code to generate a reasonable name for a function
+function concoctFunctionName (node) {
+    var name = undefined;
+
+    if (node.type === 'FunctionDeclaration') {
+        // function xxx() { }
+        //  -> "xxx"
+        name = node.id.name;
+    } else if (node.type === 'FunctionExpression') {
+        if (node.id) {
+            // (function xxx() { })
+            //  -> "xxx"
+            name = node.id.name;
+        } else if (node.parent.type === 'VariableDeclarator') {
+            // var xxx = function () { }
+            //  -> "xxx"
+            name = node.parent.id.name;
+        } else if (node.parent.type === 'AssignmentExpression') {
+            var left = node.parent.left;
+            if (left.type === 'MemberExpression' && !left.computed) {
+                if (left.object.type === 'MemberExpression' && !left.object.computed) {
+                    if (left.object.property.type === 'Identifier' && left.object.property.name === 'prototype') {
+                        // yyy.prototype.xxx = function () { }
+                        //  -> "yyy.xxx"
+                        name = left.object.object.name + '.' + left.property.name;
+                    }
+                }
+            }
+            else if (left.type === 'Identifier') {
+                // xxx = function () { }
+                //  -> "xxx"
+                name = left.name;
+            }
+        } else if (node.parent.type === 'CallExpression') {
+            // look, I know this is a regexp, I'm just sick of parsing ASTs
+            if (/\.on$/.test(node.parent.callee.source())) {
+                var args = node.parent.arguments;
+                if (args[0].type === 'Literal' && typeof args[0].value === 'string') {
+                    // .on('event', function () { })
+                    //  -> "('event' handler)"
+                    name = "('" + args[0].value + "' handler)";
+                }
+            } else if (node.parent.callee.type === 'Identifier') {
+                if (['setTimeout', 'setInterval'].indexOf(node.parent.callee.name) !== -1) {
+                    // setTimeout(function () { }, xxx)
+                    // setInterval(function () { }, xxx)
+                    //  -> "timer handler"
+                    name = 'timer handler';
+                    if (node.parent.arguments[1] && node.parent.arguments[1].type === 'Literal' && typeof node.parent.arguments[1].value === 'number') {
+                        // setTimeout(function () { }, 100)
+                        // setInterval(function () { }, 1500)
+                        //  -> "timer handler (100ms)"
+                        //  -> "timer handler (1.5s)"
+                        if (node.parent.arguments[1].value < 1000) {
+                            name += ' (' + node.parent.arguments[1].value + 'ms)';
+                        } else {
+                            name += ' (' + (node.parent.arguments[1].value / 1000) + 's)';
+                        }
+                    }
+                    name = '(' + name + ')';
+                } else {
+                    // xxx(function () { })
+                    //  -> "('xxx' callback)"
+                    name = "('" + node.parent.callee.source() + "' callback)";
+                }
+            } else if (node.parent.callee.type === 'MemberExpression') {
+                if (node.parent.callee.property.type === 'Identifier') {
+                    // xxx.yyy(..., function () { }, ...)
+                    //  -> "('yyy' callback)"
+                    name = "('" + node.parent.callee.property.name + "' callback)";
+                }
+            }
+        } else if (node.parent.type === 'Property') {
+            // { xxx: function () { } }
+            //  -> "xxx"
+            name = node.parent.key.name || node.parent.key.value;
+            if (name !== undefined) {
+                if (node.parent.parent.type === 'ObjectExpression') {
+                    var obj = node.parent.parent;
+                    if (obj.parent.type === 'VariableDeclarator') {
+                        // var yyy = { xxx: function () { } }
+                        //  -> "yyy.xxx"
+                        name = obj.parent.id.name + '.' + name;
+                    } else if(obj.parent.type === 'AssignmentExpression') {
+                        var left = obj.parent.left;
+                        if (left.type === 'MemberExpression' && !left.computed) {
+                            if (left.object.type === 'Identifier' && left.property.name === 'prototype') {
+                                // yyy.prototype = { xxx: function () { } }
+                                //  -> "yyy.xxx"
+                                name = left.object.name + '.' + name;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+    return name;
+};
+
+exports.concoctFunctionName = concoctFunctionName;
 exports.makeLabel = makeLabel;
 exports.s4 = s4;
 exports.makeId = makeId;
