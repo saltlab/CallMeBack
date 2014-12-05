abc0fa9173d017cc32fe4c8981bc53cc3412b6b4
Keheliya Gallaba <keheliya.gallaba@gmail.com>
2014-12-05 14:36:14 -0800

    new types for stat collection

--- stats/gitclone.sh
+++ stats/gitclone.sh
@@ -1,5 +1,27 @@
 #!/bin/sh
-while read p; do
-  git clone $p
-done <~/dev/RoundTrace/stats/top_npm.txt
-# done <~/dev/RoundTrace/stats/top_hybrid.txt
+
+    case $1 in
+    h)    type="hybrid";;
+    n)    type="npm";;
+    f)    type="frameworks";;
+    d)    type="dataviz";;
+    g)    type="games";;
+    e)    type="engines";;
+    ?)    printf "Usage: %s: [-h] [-n] [-f] [-d] [-g] [-e]\n" $0
+          exit 2;;
+    esac
+
+dir_path="$HOME/dev/RoundTrace/stats/top_${type}.txt"
+sources_path="$HOME/dev/top_${type}"
+
+if [ -d $sources_path ]; then
+    echo "$sources_path exists. so skipping the rest"
+else
+    echo "$sources_path does not exist. creating."
+    mkdir $sources_path
+    cd $sources_path
+    echo "Reading URLs from file $dir_path"
+    while read p; do
+        git clone $p
+    done <$dir_path
+fi
